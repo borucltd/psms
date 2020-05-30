51f55f07f89ddf26561c334b9727f437fb76090f
borucltd <mboruc@DESKTOP-L3J83AR.localdomain>
2020-05-30 14:08:32 +0930

    Test 3

--- app.js
+++ app.js
@@ -1,33 +1,133 @@
-const express = require("express");
-const PORT =  process.env.PORT || 8080;
-const router = express.Router();
-const app = express();
+var express = require('express'),
+  session = require('express-session'),
+  passport = require('passport'),
+  
+  swig = require('swig'),
+  SpotifyStrategy = require('./lib/passport-spotify/index').Strategy;
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+var consolidate = require('consolidate');
 
-// Parse application body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+const PORT = process.env.PORT || 8888;
 
+var appKey = '55e1da0fb3c74763b46c5507b68d4ca4';
+var appSecret = 'ce948c0683054e3e8ae9dbb7603f68c9';
 
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session. Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing. However, since this example does not
+//   have a database of user records, the complete spotify profile is serialized
+//   and deserialized.
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
 
-/// =========================
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
 
-// first route for GET
-router.get("/", function(req, res) {
-  console.log("Serving main page.");
-    
-      res.sendFile("index");
-    });
+// Use the SpotifyStrategy within Passport.
+//   Strategies in Passport require a `verify` function, which accept
+//   credentials (in this case, an accessToken, refreshToken, expires_in
+//   and spotify profile), and invoke a callback with a user object.
+// http://localhost:8888/callback
+passport.use(
+  new SpotifyStrategy(
+    {
+      clientID: appKey,
+      clientSecret: appSecret,
+      callbackURL: 'https://nameless-dawn-40326.herokuapp.com/callback'
+    },
+    function(accessToken, refreshToken, expires_in, profile, done) {
+      // asynchronous verification, for effect...
+      process.nextTick(function() {
+        // To keep the example simple, the user's spotify profile is returned to
+        // represent the logged-in user. In a typical application, you would want
+        // to associate the spotify account with a user record in your database,
+        // and return that user instead.
+        return done(null, profile);
+      });
+    }
+  )
+);
 
+var app = express();
 
+// configure Express
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
+app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+// Initialize Passport!  Also use passport.session() middleware, to support
+// persistent login sessions (recommended).
+app.use(passport.initialize());
+app.use(passport.session());
 
+app.use(express.static(__dirname + '/public'));
 
+app.engine('html', consolidate.swig);
 
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
+app.get('/', function(req, res) {
+  res.render('index.html', { user: req.user });
+});
+
+app.get('/account', ensureAuthenticated, function(req, res) {
+  res.render('account.html', { user: req.user });
+});
+
+app.get('/login', function(req, res) {
+  res.render('login.html', { user: req.user });
+});
+
+// GET /auth/spotify
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request. The first step in spotify authentication will involve redirecting
+//   the user to spotify.com. After authorization, spotify will redirect the user
+//   back to this application at /auth/spotify/callback
+app.get(
+  '/auth/spotify',
+  passport.authenticate('spotify', {
+    scope: ['user-read-email', 'user-read-private'],
+    showDialog: true
+  }),
+  function(req, res) {
+    // The request will be redirected to spotify for authentication, so this
+    // function will not be called.
+  }
+);
+
+// GET /auth/spotify/callback
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request. If authentication fails, the user will be redirected back to the
+//   login page. Otherwise, the primary route function function will be called,
+//   which, in this example, will redirect the user to the home page.
+app.get(
+  '/callback',
+  passport.authenticate('spotify', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  }
+);
+
+app.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
+app.listen(PORT,  function() {
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
-});
+})
+
+// Simple route middleware to ensure user is authenticated.
+//   Use this route middleware on any resource that needs to be protected.  If
+//   the request is authenticated (typically via a persistent login session),
+//   the request will proceed. Otherwise, the user will be redirected to the
+//   login page.
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login');
+}
--- public/script.js
+++ public/script.js
@@ -1,31 +1,22 @@
 $(function() {
 
-    $(".login").on("click", function(event) {
-         // take these from backend
-        const client_id = "55e1da0fb3c74763b46c5507b68d4ca4";
-        const secret = "ce948c0683054e3e8ae9dbb7603f68c9";
-        const redirect_uri = "https://lnxsa.com/";
-        const encoded_redirect_uri=redirect_uri.replace(/:/g,'%3A').replace(/\//g,'%2F');
-        const spotify_url =  "https://accounts.spotify.com/authorize";
-        const proxy_heroku="https://cors-anywhere.herokuapp.com/";
-        // this can be randomized
-        const state="mariusz";
-        // these can be taken from website
-        const scope = "playlist-read-private";
-        console.log("LOGIN");
 
-        let authorize_url=`${proxy_heroku}${spotify_url}?client_id=${client_id}&response_type=code&redirect_uri=${encoded_redirect_uri}&scope=${scope}$state=${state}`;
-        // STEP 1 connect to spotify
-        $.ajax(authorize_url, {
-          type: "GET",
-        }).then(
-          function(result) {
-            console.log(result);
-         }
-        );
+    // make request to backend
+    $(".login").on("click", function(event) {
+
+        $.ajax("/authorize_spotify", {
+            type: "GET",
+          }).then(
+            function(result) {
+                console.log(result);
+            }
+          );
   
     });
 });
+
+const client_id = "55e1da0fb3c74763b46c5507b68d4ca4";
+const secret = "ce948c0683054e3e8ae9dbb7603f68c9";
 
 
   
