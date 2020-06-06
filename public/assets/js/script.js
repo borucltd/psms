d9a5c8ffe65f5ba25011361157d4e92dc18cdd23
Nicole George <hello@nicolegeorge.me>
2020-06-06 10:20:19 +0930

    Front-End added
    still need to refactor the code, add styling elements to search spotify playlists.

---
+++ public/assets/js/script.js
@@ -0,0 +1,35 @@
+$(function() {
+
+    // make request to backend
+    $(".login").on("click", function(event) {
+
+        $.ajax("/authorize_spotify", {
+            type: "GET",
+          }).then(
+            function(result) {
+                console.log(result);
+            }
+          );
+  
+    });
+
+
+    $(".submitToDatabase").on("click", function(event) {
+
+      console.log('HEERE')
+      let tracks = $(".saveToDatabase").val()
+
+      console.log(tracks)
+      $.ajax("/spotify/save_tracks", {
+          type: "POST",
+
+        }).then(
+          function(result) {
+              console.log("Saved to DB");
+          }
+        );
+
+  });
+
+});
+  
